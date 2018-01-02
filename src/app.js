002f755f1b91529d5e0056d638faaaf4304b9733
Sergio Alejandro Espitia <alejo.pm92@gmail.com>
2018-01-02 08:45:30 -0500

    [update] setting up the backbone structure part II

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp options={[]}/>, document.getElementById('app'));
+ReactDOM.render(<p>My new App</p>, document.getElementById('app'));
